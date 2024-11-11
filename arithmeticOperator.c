94fb46b52faad296e9973a8c299aba61a55302c1
GOver1519 <124514705+GOver1519@users.noreply.github.com>
2024-11-11 20:33:29 +0530

    Add files via upload

---
+++ arithmeticOperator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a = 5, b = 3;
+
+    printf("Addition:", a+b);
+    printf("Subtraction:", a-b);
+    printf("Multiplication", a*b);
+    printf("Division:", a/b);
+    printf("Modulus", a%b);
+
+    return 0;
+}
